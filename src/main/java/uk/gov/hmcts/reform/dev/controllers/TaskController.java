e191b21d7dbf4579530b597becb1e35ba26fc4d5
ReeniePie <hoganirene@gmail.com>
2025-04-29 16:46:19 +0100

    chore(cors): configure CORS to allow requests from frontend
    Added @CrossOrigin annotation to TaskController to enable cross-origin requests from http://localhost:3000. This configuration allows the React frontend to communicate with the backend API during development.

--- src/main/java/uk/gov/hmcts/reform/dev/controllers/TaskController.java
+++ src/main/java/uk/gov/hmcts/reform/dev/controllers/TaskController.java
@@ -14,6 +14,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/tasks")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TaskController {
 
     private final TaskService taskService;
